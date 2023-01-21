059c8f0460686ba5db19d0586173551f6a92f99a
jaRue1 <jaruejohnson@outlook.com>
2023-01-21 14:31:30 -0500

    working through the problem

--- src/json-exporter.ts
+++ src/json-exporter.ts
@@ -3,13 +3,14 @@ import { assignCategory } from "../utils"
 
 console.log("JSON Exporter Initialized")
 const inputText = fs.readFileSync("../import/ideal-format.txt", "utf-8")
+// const inputText = fs.readFileSync("../tests/import.txt", "utf-8")
 
 const lines = inputText.split("\n")
 
 let output = []
 for (const [index, line] of lines.entries()) {
-  const [tags, title, variantPrice] = line.split(";")
-  const handle = title.toLowerCase().replace(/[^a-z0-9]+/g, "-")
+  const [tags, productName, variantName, variantPrice] = line.split(";")
+  const handle = productName.toLowerCase().replace(/[^a-z0-9]+/g, "-")
   let category = tags.substring(0, 3).toLocaleUpperCase()
   let productCategory = assignCategory(category)
   console.log("Exporting line #", index + 1)
@@ -17,11 +18,13 @@ for (const [index, line] of lines.entries()) {
     id: index,
     handle: handle,
     tags: tags,
-    title: title,
+    productName: productName,
+    variantName: variantName,
     variantPrice: variantPrice,
     category: productCategory,
   })
 }
 
 fs.writeFileSync("../export/export.json", JSON.stringify(output))
+// fs.writeFileSync("../tests/output.json", JSON.stringify(output))
 console.log("Output to JSON File Complete")
