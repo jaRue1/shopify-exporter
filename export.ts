ddcba4af63e97cacf019c07c5c63535e22252c46
jaRue1 <jaruejohnson@outlook.com>
2023-01-20 22:26:53 -0500

    working through the problem

--- export.ts
+++ export.ts
@@ -1,42 +1,42 @@
 import * as fs from "fs"
-
+import { shopifyProductTaxonomy } from "./shopify"
 const categoryMap = {
-  AD: "Adhesive",
-  AG: "Agricultural",
-  AU: "Automotive",
-  BP: "Bicycle parts",
-  CD: "Child Clothing",
-  CM: "Medial Clothing",
-  CP: "Worksite Clothing",
-  CS: "Chinese Clothing",
-  DU: "Roofing", // DUP RFZ7 should be under roofing RF,
-  EL: "Electrical", //(large category),
-  //! FB: "Food & Beverage", //(skip this),
-  FT: "Fasteners / Nails & Bolts",
-  FX: "Fixtures",
-  //! GF: "Gifts", //(skip this),
-  HH: "House Hold Items",
-  //! LI: "Baby Chickens", // (skip this) (Move to AG section),
-  //! MI: "Misc", //(skip this),
-  MS: "Misalliance Items / Multi Purpose Items",
-  MT: "Motor Tools",
-  MW: "Metal Work",
-  PA: "Paint",
-  PB: "Plumbing", //(large category),
-  //! PC: "Phone Card", //(skip this),
-  //! PH: "Personal Hygiene", //(skip this),
-  RF: "Roofing",
-  RM: "Raw Materials",
-  //! SG: "Sport Game", //(skip this),
-  SH: "Second Hand Items (Used)",
-  //! ST: "Station", //(skip this),
-  TI: "Tiling",
-  TO: "Tools", // (large category),
-  WP: "Wood Products / Lumber / Plyboards",
+  AD: "Adhesive", // shopifyProductTaxonomy.key2183: `Hardware > Building Consumables > Hardware Glue & Adhesives`,
+  AG: "Agricultural", // shopifyProductTaxonomy.key3038: `Home & Garden`
+  AU: "Automotive", // shopifyProductTaxonomy.key5352: `Vehicles & Parts`
+  BP: "Bicycle parts", //  shopifyProductTaxonomy.key4890: `Sporting Goods > Outdoor Recreation > Cycling > Bicycle Parts > Bicycle Brake Parts`,
+  CD: "Child Clothing", // shopifyProductTaxonomy.key148: `Apparel & Accessories > Clothing > Baby & Toddler Clothing`,
+  CM: "Medical Clothing", // shopifyProductTaxonomy.key1153: `Business & Industrial > Work Safety Protective Gear > Protective Masks`,
+  CP: "Worksite Clothing", // shopifyProductTaxonomy.key1146: `Business & Industrial > Work Safety Protective Gear`,
+  CS: "Chinese Clothing", // shopifyProductTaxonomy.key352: `Apparel & Accessories > Shoes`,
+  DU: "Roofing", // shopifyProductTaxonomy.key2225: `Hardware > Building Materials > Roofing`,
+  EL: "Electrical", //   shopifyProductTaxonomy.key2416: `Hardware > Power & Electrical Supplies`,
+  FT: "Fasteners", //   shopifyProductTaxonomy.key2283: `Hardware > Hardware Accessories > Hardware Fasteners`,
+  FTB: "Bolts", // shopifyProductTaxonomy.key2286: `Hardware > Hardware Accessories > Hardware Fasteners > Nuts & Bolts`
+  FTN: "Nails", // shopifyProductTaxonomy.key2285: `Hardware > Hardware Accessories > Hardware Fasteners > Nails`,
+  FX: "Fixtures", //  shopifyProductTaxonomy.key2170: `Hardware`,
+  HH: "House Hold Items", // shopifyProductTaxonomy.key3303: `Home & Garden > Household Supplies`,
+  MS: "Misalliance Items / Multi Purpose Items", // shopifyProductTaxonomy.key3303: `Home & Garden > Household Supplies`,
+  MT: "Motor Tools", //   shopifyProductTaxonomy.key5352: `Vehicles & Parts`,
+  MW: "Metal Work", // shopifyProductTaxonomy.key2170: `Hardware`,
+  PA: "Paint", //  shopifyProductTaxonomy.key2191: `Hardware > Building Consumables > Painting Consumables > Paint`,
+  PB: "Plumbing", //   shopifyProductTaxonomy.key2335: `Hardware > Plumbing`,
+  RF: "Roofing", //   shopifyProductTaxonomy.key2225: `Hardware > Building Materials > Roofing`,
+  RM: "Raw Materials", // shopifyProductTaxonomy.key2201: `Hardware > Building Materials`,
+  SH: "Second Hand Items (Used)", //  shopifyProductTaxonomy.key2170: `Hardware`,
+  TI: "Tiling", // shopifyProductTaxonomy.key2229: `Hardware > Building Materials > Roofing > Roofing Shingles & Tiles`,
+  TO: "Tools", // shopifyProductTaxonomy.key2516: `Hardware > Tools`,
+  WP: "Wood Products / Lumber / Plyboards", //   key2222: `Hardware > Building Materials > Lumber & Sheet Stock`,
 }
 function assignCategory(category: string): string {
   let assignedCategory = ""
-  switch (category) {
+  if (category === "FTB") {
+    assignedCategory = categoryMap.FTB
+  }
+  if (category === "FTN") {
+    assignedCategory = categoryMap.FTN
+  }
+  switch (category.substring(0, 2)) {
     case "AD":
       assignedCategory = categoryMap.AD
       break
@@ -70,27 +70,15 @@ function assignCategory(category: string): string {
     case "EL":
       assignedCategory = categoryMap.EL
       break
-    //! case "FB":
-    //!   assignedCategory = catagoryMap.FB
-    //!   break
     case "FT":
       assignedCategory = categoryMap.FT
       break
     case "FX":
       assignedCategory = categoryMap.FX
       break
-    //! case "GF":
-    //!   assignedCategory = catagoryMap.GF
-    //!   break
     case "HH":
       assignedCategory = categoryMap.HH
       break
-    //! case "LI":
-    //!   assignedCategory = catagoryMap.LI
-    //!   break
-    //! case "MI":
-    //!   assignedCategory = catagoryMap.MI
-    //!   break
     case "MS":
       assignedCategory = categoryMap.MS
       break
@@ -106,30 +94,18 @@ function assignCategory(category: string): string {
     case "PA":
       assignedCategory = categoryMap.PA
       break
-    //! case "PC":
-    //!   assignedCategory = catagoryMap.PC
-    //!   break
     case "PB":
       assignedCategory = categoryMap.PB
       break
-    //! case "PH":
-    //!   assignedCategory = catagoryMap.PH
-    //!   break
     case "RF":
       assignedCategory = categoryMap.RF
       break
     case "RM":
       assignedCategory = categoryMap.RM
       break
-    //! case "SG":
-    //!   assignedCategory = catagoryMap.SG
-    //!   break
     case "SH":
       assignedCategory = categoryMap.SH
       break
-    //! case "ST":
-    //!  assignedCategory = catagoryMap.ST
-    //!   break
     case "TI":
       assignedCategory = categoryMap.TI
       break
@@ -166,7 +142,7 @@ for (const [index, line] of lines.entries()) {
   // Generate a handle from the title
   const handle = title.toLowerCase().replace(/[^a-z0-9]+/g, "-")
   // Grab the first two letters from title
-  let category = tags.substring(0, 2).toLocaleUpperCase()
+  let category = tags.substring(0, 3).toLocaleUpperCase()
   console.log("category:", category, "title:", title, "line #:", index)
 
   // Set the other fields to empty values
@@ -212,7 +188,6 @@ console.log("Export to output.csv complete.")
 // * Phase 1 categorize data
 // ? How can I dynamically categorize the data ?
 // TODO : Build a map and use the first two letters in the tags to find the correct product category
-// * Complete !
 
 // * Phase 2 account for variable product ( up next )
 // ? How can I
